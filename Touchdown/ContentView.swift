f68ed7184c0a845d2ce501e85c1820b317a6f352
AlexBQ7 <alejandro.barreto.quintana@gmail.com>
2022-11-16 14:26:22 -0600

    Initial Commit

---
+++ Touchdown/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Touchdown
+//
+//  Created by David Alejandro Ruiz Hernandez on 16/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by David Alejandro Ruiz Hernandez on 16/11/22.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
